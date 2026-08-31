64a60f1efd59fefb2de6fbfecbd3771ca485a568
IshanntNaredi <narediishant@gmail.com>
2026-09-01 01:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.21%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(int i=0;i<word.size();i++){
+            if(isupper(word[i])){
+                count++;
+            }
+        }
+        return count == 0 ||
+               (count == 1 && isupper(word[0])) ||
+               count == word.size();
+    }
+};
